7682e863f80004656f364d01d891eccfcb118ef7
habil mahendri <habilmahendri@habils-MacBook-Pro.local>
2018-10-07 21:31:15 +0700

    drawer

--- app/src/main/java/com/example/habilmahendri/popcorn/HabilActivity.java
+++
@@ -1,4 +0,0 @@
-package com.example.habilmahendri.popcorn;
-
-public class HabilActivity {
-}
--- app/src/main/java/com/example/habilmahendri/popcorn/MainActivity.java
+++ app/src/main/java/com/example/habilmahendri/popcorn/MainActivity.java
@@ -71,17 +71,19 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.movie) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.tv_show) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.dicover) {
 
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
+
+        } else if (id == R.id.pop_people) {
 
         }
 
