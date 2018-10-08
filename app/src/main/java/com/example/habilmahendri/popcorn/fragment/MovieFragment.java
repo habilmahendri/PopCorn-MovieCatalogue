38a9fd1030ec7efd3a7075c92c688fbd68d5015c
habil mahendri <habilmahendri@habils-MacBook-Pro.local>
2018-10-08 20:33:23 +0700

    add design testing

--- app/src/main/java/com/example/habilmahendri/popcorn/fragment/MovieFragment.java
+++ app/src/main/java/com/example/habilmahendri/popcorn/fragment/MovieFragment.java
@@ -2,18 +2,29 @@ package com.example.habilmahendri.popcorn.fragment;
 
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toolbar;
 
 import com.example.habilmahendri.popcorn.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MovieFragment extends Fragment {
+
+
+    @BindView(R.id.toolbar)Toolbar toolbar;
+    @BindView(R.id.viewpager) ViewPager viewPager;
+    @BindView(R.id.tabs) TabLayout tabLayout;
 
 
     public MovieFragment() {
@@ -24,8 +35,11 @@ public class MovieFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_movie, container, false);
+
+        ButterKnife.bind(this,view);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie, container, false);
+        return view;
     }
 
 }
